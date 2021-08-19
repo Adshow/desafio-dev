645124ba084e62b83f80bf0554968275cc8a24fa
Adson Santos Cardoso <adson.cardoso@defensoria.ba.def.br>
2021-08-19 20:06:01 -0300

    get transaction infos from line

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -3,21 +3,46 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Repositories\TransacaoRepository;
 
 
 class FileController extends Controller
 {
+    public function __construct(TransacaoRepository $TransacaoRepository)
+    {
+        $this->TransacaoRepository = $TransacaoRepository;
+    }
+    
     function parserCnab(Request $request)
     {
+        try
+        {
+            $file_handle = $this->openFile($request->file('file'));
+        
+            while(!feof($file_handle)){
 
+                $line = fgets($file_handle);
+                
+                $transacao = array();
 
-        $file_handle = $this->openFile($request->file('file'));
-    
-
-        while(!feof($file_handle)){
-
-            $line = fgets($file_handle);
-            dd($line);
+                $transacao['tipo']= $this->getInfo($line, 0, 1);
+                $transacao['data']= $this->getInfo($line, 1, 8);
+                $transacao['valor']= $this->normalize($this->getInfo($line, 9, 10));
+                $transacao['cpf']= $this->getInfo($line, 19, 11);
+                $transacao['cartao']= $this->getInfo($line, 30, 12);
+                $transacao['hora']= $this->getInfo($line, 42, 6);
+                $transacao['dono']= trim($this->getInfo($line, 48, 14));
+                $transacao['loja']= trim($this->getInfo($line, 62, 19));
+                
+                
+                
+                dd($line, $transacao);
+            }
+        }
+        catch(\Exception $e)
+        {
+            dd($e);
+            return response($e->getMessage(), $e->getStatusCode());
         }
 
         
@@ -28,5 +53,15 @@ class FileController extends Controller
         $file->move('uploads', $file->getClientOriginalName());
 
         return fopen("uploads/".$file->getClientOriginalName(), 'r');
+    }
+
+    function getInfo($string, $start, $length)
+    {
+        return substr($string, $start, $length);
+    }
+
+    function normalize($value)
+    {
+        return $value/100.00;
     }
 }
---
+++ app/Repositories/TransacaoRepository.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Repositories;
+
+
+
+class TransacaoRepository
+{
+    public function store($request)
+    {
+    }
+}
